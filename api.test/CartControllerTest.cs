bb62f38922b67cb909833f21750d7530059fc45c
Nazariy-228 <docnazar7@gmail.com>
2021-08-02 15:49:52 +0300

    initial

---
+++ api.test/CartControllerTest.cs
@@ -0,0 +1,94 @@
+// CartControllerTest.cs
+
+using System;
+using Services;
+using Moq;
+using api.Controllers;
+using System.Linq;
+using System.Collections.Generic;
+using AutoFixture;
+using AutoFixture.AutoMoq;
+using AutoFixture.Xunit2;
+using FluentAssertions;
+using Xunit;
+
+namespace test
+{
+    public class AutoDomainDataAttribute : AutoDataAttribute
+    {
+        public AutoDomainDataAttribute()
+            :base(() => new Fixture().Customize(new AutoMoqCustomization()))
+        {
+        }
+    }
+  public class Tests
+  {
+      private CartController controller;
+
+      [Theory]
+      [AutoDomainData]
+      public void CheckOutShouldReturnCharged(
+          [Frozen] Mock<IPaymentService> paymentServiceMock, 
+          [Frozen] Mock<IShipmentService> shipmentServiceMock,
+          [Frozen] Mock<ICartService> cartServiceMock, 
+          [Frozen] Mock<ICard> cardMock, 
+          [Frozen] Mock<IAddressInfo> addressInfoMock,
+          [Frozen] Mock<CartItem> cartItemMock, 
+          List<CartItem> items)
+      {
+          cartItemMock.Setup(item => item.Price)
+              .Returns(20);
+          
+          cartServiceMock.Setup(c => c.Items())
+              .Returns(items.AsEnumerable());
+          
+          controller = new CartController(
+              cartServiceMock.Object, paymentServiceMock.Object, shipmentServiceMock.Object);
+          
+          paymentServiceMock.Setup(p => 
+                  p.Charge(It.IsAny<double>(), cardMock.Object))
+              .Returns(true);
+
+          // act
+          var result = controller.CheckOut(cardMock.Object, addressInfoMock.Object);
+
+          // assert
+          shipmentServiceMock.Verify(s => 
+              s.Ship(addressInfoMock.Object, items.AsEnumerable()), Times.Once());
+
+          result.Should().Be("charged");
+      }
+
+      [Theory]
+      [AutoDomainData]
+      public void CheckOutShouldReturnNotCharged(
+          [Frozen] Mock<IPaymentService> paymentServiceMock, 
+          [Frozen] Mock<IShipmentService> shipmentServiceMock,
+          [Frozen] Mock<ICartService> cartServiceMock, 
+          [Frozen] Mock<ICard> cardMock, 
+          [Frozen] Mock<IAddressInfo> addressInfoMock,
+          [Frozen] Mock<CartItem> cartItemMock, 
+          List<CartItem> items)
+      {
+          cartItemMock.Setup(item => item.Price)
+              .Returns(20);
+          
+          cartServiceMock.Setup(c => c.Items())
+              .Returns(items.AsEnumerable());
+          
+          controller = new CartController(
+              cartServiceMock.Object, paymentServiceMock.Object, shipmentServiceMock.Object);
+          
+          paymentServiceMock.Setup(p => 
+              p.Charge(It.IsAny<double>(), cardMock.Object)).Returns(false);
+
+          // act
+          var result = controller.CheckOut(cardMock.Object, addressInfoMock.Object);
+
+          // assert
+          shipmentServiceMock.Verify(s => 
+              s.Ship(addressInfoMock.Object, items.AsEnumerable()), Times.Never());
+          result.Should().Be("not charged");
+      }
+  }
+}
